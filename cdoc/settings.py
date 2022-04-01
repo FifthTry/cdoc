f5dc601da8dc5d546faba4c21c4e63aae181cad1
Shobhit <shobhit@fifthtry.com>
2022-04-01 15:01:28 +0530

    Cleanup + move few things to env variables

--- cdoc/settings.py
+++ cdoc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os, dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,17 +77,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "cdoc.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
@@ -207,12 +197,22 @@ LOGGING = {
     },
 }
 
-APP_AUTH_PEM_FILE = BASE_DIR.joinpath("PRIVATE_KEY.pem")
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+DATABASES = dict()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES["default"] = dj_database_url.config(DATABASE_URL)
+else:
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+
+APP_AUTH_KEY = os.getenv("APP_AUTH_KEY")
 try:
     from .local_settings import *  # noqa
 except ModuleNotFoundError:
     pass
-APP_AUTH_KEY = None
-with open(APP_AUTH_PEM_FILE, "r") as pem_file:
-    APP_AUTH_KEY = pem_file.read().encode()
